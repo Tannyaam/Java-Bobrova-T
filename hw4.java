14c5166d68d303c41925c9caa02331ee2fa21440
Tatiana <www.t97@mail.ru>
2022-11-13 12:11:58 +0300

    добавлена задача по дз 4

---
+++ hw4.java
@@ -0,0 +1,39 @@
+import java.util.Random;
+import java.util.List;
+
+public class hw4 {
+    public static void main(String[] args) {
+        int [] array = {1, 8, 4, -3, 2, 8, 6, 4, 5};
+        quicksort(array, 0, array.length - 1);
+        for (int i = 0; i < array.length; i++) {
+            System.out.printf(array[i] + " ");
+        }
+    }
+
+    public static int partition (int[] array, int start, int end) 
+   {
+       int marker = start;
+       for ( int i = start; i <= end; i++ ) 
+       {
+           if ( array[i] <= array[end] ) 
+           {
+               int temp = array[marker]; // swap
+               array[marker] = array[i];
+               array[i] = temp;
+               marker += 1;
+           }
+       }
+       return marker - 1;
+   }
+  
+   public static void quicksort (int[] array, int start, int end)
+   {
+       if ( start >= end ) 
+       {
+           return;
+       }
+       int pivot = partition (array, start, end);
+       quicksort (array, start, pivot-1);
+       quicksort (array, pivot+1, end);
+   }
+}
